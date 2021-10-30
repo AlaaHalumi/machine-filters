c4674ac302cb75c1af8456332a83f83f049982ff
Alaa Halumi <alaa@justmanage.com>
2021-10-30 16:04:57 +0300

    changes

--- src/app/core/models/machine-status.ts
+++ src/app/core/models/machine-status.ts
@@ -1,7 +1,7 @@
 export enum MachineStatus {
-    acceptable = 0,
-    monitor = 1,
-    alarm = 2,
-    danger = 3,
-    noStatus= 4
+    acceptable = 10,
+    monitor = 20,
+    alarm = 30,
+    danger = 40,
+    noStatus= 50
 }
